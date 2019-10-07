26b3d8895e57c688cafa1577e5b901d090a5b6c4
Gabriel Rodrigues <38117500+Gabriel1011@users.noreply.github.com>
2019-10-07 20:23:28 -0300

    Adicionando Logging

---
+++ testeLogging/testeLogging/Controllers/TesteLoggingController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
+namespace testeLogging.Controllers
+{
+    [Route("Teste")]    
+    public class TesteLoggingController : Controller
+    {
+        private readonly ILogger _logger;
+        public TesteLoggingController(ILogger<TesteLoggingController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("TesteLogging")]
+        public async Task<IActionResult> TesteLogging()
+        {
+            _logger.LogInformation("Método chamado com sucesso!!!");
+            _logger.LogDebug("Método chamado com sucesso!!!");
+            _logger.LogWarning("Método chamado com sucesso!!!");
+            _logger.LogError("Método chamado com sucesso!!!");
+            _logger.LogCritical("Método chamado com sucesso!!!");
+
+            return Ok();
+        }
+
+        [HttpGet("OutroTeste")]
+        public void teste()
+        {
+
+        }
+    }
+}
