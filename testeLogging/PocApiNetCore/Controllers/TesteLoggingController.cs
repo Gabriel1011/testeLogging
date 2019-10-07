27c8eb39b436d34e12f1777fd9bbdd58532f164d
Gabriel Rodrigues <38117500+Gabriel1011@users.noreply.github.com>
2019-10-07 20:39:17 -0300

    Novo projeto para testes de ferramentas.

---
+++ testeLogging/PocApiNetCore/Controllers/TesteLoggingController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace PocApiNetCore.Controllers
+{
+    [Route("Teste")]
+    public class TesteLoggingController : Controller
+    {
+        private readonly ILogger _logger;
+        public TesteLoggingController(ILogger<TesteLoggingController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("TesteLogging")]
+        public async Task<IActionResult> TesteLogging()
+        {
+            _logger.LogInformation("Método chamado com sucesso!!!");
+            _logger.LogDebug("Método chamado com sucesso!!!");
+            _logger.LogWarning("Método chamado com sucesso!!!");
+            _logger.LogError("Método chamado com sucesso!!!");
+            _logger.LogCritical("Método chamado com sucesso!!!");
+
+            return Ok();
+        }
+
+        [HttpGet("OutroTeste")]
+        public void teste()
+        {
+
+        }
+    }
+}
---
+++ testeLogging/PocApiNetCore/Startup.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Swashbuckle.AspNetCore.Swagger;
+
+namespace PocApiNetCore
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "TesteLoggingAPI", Version = "v1" });
+                //var xmlPath = System.AppDomain.CurrentDomain.BaseDirectory + "testeLogging.xml";
+                //c.IncludeXmlComments(xmlPath);
+            });
+
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseSwagger();
+
+            // Ativa o Swagger UI
+            app.UseSwaggerUI(opt =>
+            {
+                opt.SwaggerEndpoint("/swagger/v1/swagger.json", "TesteLoggingAPI V1");
+            });
+
+            app.UseMvc();
+        }
+    }
+}
